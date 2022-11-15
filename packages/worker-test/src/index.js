8f806be4040c2b5f783939a57727b2d3a886bf71
Lukas Reinfurt <github@mail.decouplr.com>
2022-11-15 19:45:14 +0100

    feat: integrate sentry into worker-test package

--- packages/worker-test/src/index.js
+++ packages/worker-test/src/index.js
@@ -1,10 +1,39 @@
-async function handleRequest(request) {
-	console.log(JSON.stringify(request));
-	return new Response(JSON.stringify({ received: true }), {
-		headers: { 'Content-type': 'application/json' }
-	});
-}
+import Toucan from 'toucan-js';
+import { version } from '../package.json';
 
 addEventListener('fetch', (event) => {
+	const environment =  NODE_ENV;
+	let sentry;
+
+	async function handleRequest(request) {
+		try {
+			const body = await request.json();
+			if(body.fail) {
+				callUndefinedFunction();
+			}
+			return new Response(JSON.stringify(body), {
+				headers: { 'Content-type': 'application/json' }
+			});
+		} catch (error) {
+			if(environment != 'development') {
+				if (!sentry) {
+					sentry = new Toucan({
+						dsn: SENTRY_DSN,
+						context: event,
+						environment: environment,
+						release: version
+					});
+				}
+				sentry.captureException(error);
+				return new Response('Something went wrong', {
+					status: 500,
+					statusText: 'Internal Server Error',
+				});
+			} else {
+				throw error;
+			}
+		}
+	}
+
 	event.respondWith(handleRequest(event.request));
 });
