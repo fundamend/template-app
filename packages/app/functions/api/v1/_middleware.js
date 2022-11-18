b0439f2cd7afcffe43578bcce58fba25154fe5e2
Lukas Reinfurt <github@mail.decouplr.com>
2022-11-18 14:46:52 +0100

    chore: clean up code

--- packages/app/functions/api/v1/_middleware.js
+++ packages/app/functions/api/v1/_middleware.js
@@ -5,21 +5,20 @@ const reportCrash = async (context) => {
 	try {
 		return await context.next();
 	} catch (error) {
-		console.log('#### CrashReporter');
-		if(context.env.NODE_ENV != 'development') {
-		const crashReporter = new CrashReporter({
-			context: context,
-			dsn: context.env.SENTRY_DSN,
-			environment: context.env.NODE_ENV,
-			release: pkg.version
-		});
-		crashReporter.captureException(error);
-		return new Response('Something went wrong', {
-			status: 500,
-			statusText: 'Internal Server Error'
-		});
+		if (context.env.NODE_ENV != 'development') {
+			const crashReporter = new CrashReporter({
+				context: context,
+				dsn: context.env.SENTRY_DSN,
+				environment: context.env.NODE_ENV,
+				release: pkg.version
+			});
+			crashReporter.captureException(error);
+			return new Response('Something went wrong', {
+				status: 500,
+				statusText: 'Internal Server Error'
+			});
 		} else {
-	  	throw error;
+			throw error;
 		}
 	}
 };
