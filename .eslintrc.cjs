d1a3a4e5060e43bde444f2e416aca00ed225bce6
- <gitkraken@mail.decouplr.com>
2023-02-08 20:17:57 +0100

    refactor: pull eslint and prettier plugins into base configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,6 @@
-/* TODO: import config-eslint
-const fundamend = require('@fundamend/config-eslint');
+const config = require('@fundamend/config-eslint');
 
 module.exports = {
-	...fundamend
-};
-*/
-
-module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'prettier', 'plugin:astro/recommended'],
-	plugins: ['svelte3'],
-	overrides: [
-		{ files: ['*.svelte'], processor: 'svelte3/svelte3' },
-		{ files: ['*.astro'], parser: 'astro-eslint-parser' }
-	],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 'latest'
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+	...config,
+	root: true
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,5 @@
 const config = require('@fundamend/config-prettier');
 
 module.exports = {
-	...config,
-	plugins: ['prettier-plugin-svelte'],
-	pluginSearchDirs: ['.'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+	...config
 };
